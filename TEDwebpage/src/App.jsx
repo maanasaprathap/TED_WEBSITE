5dcd3c8bc4390e18d03529f867e14f056e7570fd
765sanjay <sanjaysrc765@gmail.com>
2025-02-18 13:57:45 +0530

    Events page done

--- TEDwebpage/src/App.jsx
+++ TEDwebpage/src/App.jsx
@@ -6,13 +6,20 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Events from "./pages/Events";
 import Speakers from "./pages/Speakers";
-import JoinUs from "./pages/JoinUs";
 import Contact from "./pages/Contact";
 import Gallery from "./pages/Gallery";
 import Feminism from "./components/Article_Pages/Feminism";
 import ComfortZoneArticle from "./components/Article_Pages/Comfort_zone";
 import DNA_Article from "./components/Article_Pages/DNA";
 import ImposterSyndromeArticle from "./components/Article_Pages/Imposter_syndrome";
+import { Innovate_18 } from "./components/Events_Articles/innovate'18";
+import { Innovate_19 } from "./components/Events_Articles/innovate'19";
+import { Innovate_19_2 } from "./components/Events_Articles/innovate'19.2.0";
+import { Innovate_21 } from "./components/Events_Articles/innovate'21";
+import { Innovate_22 } from "./components/Events_Articles/innovate'22";
+import { Innovate_23 } from "./components/Events_Articles/innovate'23";
+import { Innovate_24 } from "./components/Events_Articles/innovate'24";
+import { Orginate_24 } from "./components/Events_Articles/orginate'24";
 
 function App() {
   return (
@@ -23,13 +30,21 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/events" element={<Events />} />
         <Route path="/speakers" element={<Speakers />} />
-        <Route path="/join-us" element={<JoinUs />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/feminism" element={<Feminism />} />
         <Route path="/comfort-zone" element={<ComfortZoneArticle />} />
         <Route path="/dna-storage" element={<DNA_Article />} />
         <Route path="/imposter-syndrome" element={<ImposterSyndromeArticle />} />
+        <Route path="/innovate'18" element={<Innovate_18 />} />
+        <Route path="/innovate'19" element={<Innovate_19 />} />
+        <Route path="/innovate'19.2.0" element={<Innovate_19_2 />} />
+        <Route path="/innovate'21" element={<Innovate_21 />} />
+        <Route path="/innovate'22" element={<Innovate_22 />} />
+        <Route path="/innovate'23" element={<Innovate_23 />} />
+        <Route path="/innovate'24" element={<Innovate_24 />} />
+        <Route path="/orginate'24" element={<Orginate_24 />} />
+
       </Routes>
       <Footer />
     </Router>
---
+++ TEDwebpage/src/components/Events_Articles/innovate'22.jsx
@@ -0,0 +1,10 @@
+export const Innovate_22 = () =>{
+    return(
+    <div className="p-8 bg-black min-h-screen flex flex-col overflow-hidden relative">
+      <h1 className="text-3xl font-bold text-red-600">Innovate 2022</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+       
+      </div>
+    </div>
+    );
+}
